e4d6e21787e97a01f3efc9a87e96af12697aef38
santaev <santaevp@gmail.com>
2018-03-13 22:57:49 +0700

    added gradle project

---
+++ src/main/kotlin/ru/monopolio/quiz/Main.kt
@@ -0,0 +1,22 @@
+package ru.monopolio.quiz
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.jetty.*
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Jetty, port = 9000) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
